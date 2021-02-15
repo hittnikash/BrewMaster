42cc8cb66778db254adf95cb593b9ad0e6fe7fc1
ashley needy <ashleyneedy@yahoo.com>
2021-02-15 07:54:39 -0500

    refactor ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -1,7 +1,7 @@
 const urlRandom = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
 const error = 'Error. There was a problem retrieving data. Status Code: ';
 const btn = document.getElementById('search');
-
+const div = document.querySelector('#randomDiv');
 
 function randomCocktail() {                              
 fetch(urlRandom)
@@ -13,47 +13,36 @@ fetch(urlRandom)
    }                                 
       });
     }
-  )
+  );
 }
 randomCocktail();
 
+function createElement(elementName, property, value) {   ///create my elements/properties & values.
+    const element = document.createElement(elementName);
+    element[property] = value;
+    return element;
+    }
 
+function appendElements(elementName, property, value) {
+    const element = createElement(elementName, property, value);
+    div.appendChild(element); 
+    }
+    
 function displayRandomCocktail(cocktail) {
     console.log(cocktail.drinks[0]);
-
-        function createElement(elementName, property, value) {
-            const element = document.createElement(elementName);
-            element[property] = value;
-            return element;
-        }    
   
-const div = document.querySelector('#randomDiv');
-const beverageName = document.createElement('h3');
-beverageName.innerHTML = cocktail.drinks[0].strDrink;
-div.appendChild(beverageName);
+    
+    appendElements('h3', 'innerHTML', `${cocktail.drinks[0].strDrink}`);
+    appendElements('img', 'src', `${cocktail.drinks[0].strDrinkThumb}`);
+    
 
-
-const img = document.createElement('img');
-img.src = cocktail.drinks[0].strDrinkThumb;
-div.appendChild(img);
-
-
-
-for(let i = 1; i < 16; i++) {
-    console.log(i);
-    if(cocktail.drinks[0][`strIngredient${i}`] || cocktail.drinks[0][`strMeasure${i}`] === null || cocktail.drinks[0][`strIngredient${i}`] || cocktail.drinks[0][`strMeasure${i}`]=== ' ') {
-        break;
+    for(let i = 1; i < 16; i++) {
+        console.log(i);
+        if(cocktail.drinks[0][`strIngredient${i}`]  === null || cocktail.drinks[0][`strIngredient${i}`] === ' ') {
+            break;
+        }
+    appendElements('li', 'innerHTML', `${cocktail.drinks[0][`strMeasure${i}`] +': ' + cocktail.drinks[0][`strIngredient${i}`]}`);
     }
-const list = document.createElement('ul');
-const ingredients = document.createElement('li');
-ingredients.innerHTML = cocktail.drinks[0][`strMeasure${i}`] +': ' + cocktail.drinks[0][`strIngredient${i}`];
-div.appendChild(list);
-div.appendChild(ingredients);
 
-};
-
-const instructions = document.createElement('p');
-instructions.innerHTML = cocktail.drinks[0].strInstructions;
-div.appendChild(instructions)
-}
-
+    appendElements('p', 'innerHTML', `${cocktail.drinks[0].strInstructions}`);
+    }
