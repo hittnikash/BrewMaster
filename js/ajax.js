1d29a1e1045391868e8709fad0f505a2b38b2468
ashley needy <ashleyneedy@yahoo.com>
2021-02-21 05:21:32 -0500

    mobile-design-styles

--- js/ajax.js
+++ js/ajax.js
@@ -1,51 +1,67 @@
 const urlRandom = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
 const error = 'Error. There was a problem retrieving data. Status Code: ';
-const btn = document.getElementById('search');
 const div = document.querySelector('#randomDiv');
-const divText = document.querySelector('#textDiv');
 const btnTry = document.getElementById('btn');
 
-btnTry.addEventListener('click', () => {
-    window.location.reload();
-})
+  btnTry.addEventListener('click', () => {
+      window.location.reload();
+  })
 
-function randomCocktail(event) {                              ///fetch request/response/status
-fetch(urlRandom)
-  .then((response) => { response.json()                  
-  .then((data) => {displayRandomCocktail(data);    
-    if (response.status !== 200) {
-    console.log(`${error} response.status`);
-    return;
-   }                                 
-      });
-    }
-  );
-}
-randomCocktail();
+  function randomCocktail() {                              ///fetch request/response/status
+  fetch(urlRandom)
+    .then((response) => { response.json()                  
+    .then((data) => {displayRandomCocktail(data);    
+      if (response.status !== 200) {
+      console.log(`${error} response.status`);
+      return;
+    }                                 
+        });
+      }
+    );
+  }
+  randomCocktail();
 
-function createElement(elementName, property, value) {   ///create my elements/properties & values.
-    const element = document.createElement(elementName);
-    element[property] = value;
-    return element;
-    }
+  function createElement(elementName, property, value) {   ///create my elements/properties & values.
+      const element = document.createElement(elementName);
+      element[property] = value;
+      return element;
+      }
 
-function appendElements(elementName, property, value) {      /// append my elements/properties & values.
-    const element = createElement(elementName, property, value);
-    div.appendChild(element); 
-    }
-    
-function displayRandomCocktail(cocktail) {                    /// display/ append drink name, and photo
-   console.log(cocktail.drinks[0]);
-    appendElements('h3', 'innerHTML', `${cocktail.drinks[0].strDrink}`);
-    appendElements('img', 'src', `${cocktail.drinks[0].strDrinkThumb}`);
-     for(let i = 1; i < 16; i++) {                      /// loop through the ingredients to get rid of listed 'null'
-        console.log(i);                                /// & empty strings.
-        if(cocktail.drinks[0][`strIngredient${i}`]  === null || cocktail.drinks[0][`strIngredient${i}`] === ' ') {  
-            break;
-        }
+  function appendElements(elementName, property, value) {      /// append my elements/properties & values.
+      const element = createElement(elementName, property, value);
+      div.appendChild(element); 
+      }
+      
+  function displayRandomCocktail(cocktail) {                    /// display/ append drink name, and photo
+    console.log(cocktail.drinks[0]);
+      appendElements('h3', 'innerHTML', `${cocktail.drinks[0].strDrink}`);
+      appendElements('img', 'src', `${cocktail.drinks[0].strDrinkThumb}`);
+          for(let i = 1; i < 16; i++) {                      /// loop through the ingredients to get rid of listed 'null'
+              console.log(i);                                /// & empty strings.
+              if(cocktail.drinks[0][`strIngredient${i}`]  === null || cocktail.drinks[0][`strIngredient${i}`] === '') {  
+                break;
+              } else if (cocktail.drinks[0][`strMeasure${i}`]  === null || cocktail.drinks[0][`strMeasure${i}`] === '') {  
+                break;
+              }
+      appendElements('li', 'innerHTML', `${cocktail.drinks[0][`strMeasure${i}`] +': ' + cocktail.drinks[0][`strIngredient${i}`]}`);}  // measurements
+      appendElements('p', 'innerHTML', `${cocktail.drinks[0].strInstructions}`);}  //instructions
+      
+      
+  ///-------------------2nd Request------------------------------------///
 
-    appendElements('li', 'innerHTML', `${cocktail.drinks[0][`strMeasure${i}`] +': ' + cocktail.drinks[0][`strIngredient${i}`]}`);  // measurements
-   }
+// const urlSearch = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+// const searchDiv = document.getElementById('searchDiv');
 
-    appendElements('p', 'innerHTML', `${cocktail.drinks[0].strInstructions}`);  //instructions
-    }
+//   function searchForDrink() {                              ///fetch request/response/status
+//   fetch(urlRandom)
+//     .then((response) => { response.json()                  
+//     .then((data) => {searchForDrink(data);    
+//       if (response.status !== 200) {
+//       console.log(`${error} response.status`);
+//       return;
+//     }                                 
+//         });
+//       }
+//     );
+//   }
+//   searchForDrink();
--- js/productProgram.js
+++ js/productProgram.js
@@ -1,19 +1,59 @@
 let html = '';
 const content = document.querySelector('main');
+let i = 0;
 
-products.forEach((product) => {
+products.forEach((product, i) => {
 html += `
 
     <img class = "productImg" src="${product.image}" alt="${product.apparelType}"> 
     <h2 class= "apparel">${product.apparelType} | ${product.productID}</h2>
     <h4 class= "description">${product.description}</h2>
     <h2 class= "price">${product.price}</h2>
-    <div class= "btnDiv"><button id="btnPro">Add</button>
-    <button id="btnProRem">Remove</button></div>
+    
+    <div class= "btnDiv"><button type= "button" value= "${product.price}" id="btnPro">Add</button> 
+    <button type= "button" value= "${product.price}"id="btnProRem">Remove</button></div>   
 
 `;
 
-})
+ 
+    // click on item add button that items product.price to the cart.
+   
+    content.insertAdjacentHTML('beforeend', html);
+    
+    const cartArr = [];
+    const removeBtn = document.getElementById('btnProRem');
+    const addBtn = document.getElementById('btnPro');
+    const cart = document.getElementById('cart');
+    
 
-content.insertAdjacentHTML('beforeend', html);
+
+
+    addBtn.addEventListener('click', () => {
+        const prices = products.filter(product =>  {
+        cartArr.push(`${product.price}`);
+        console.log(cartArr);
+        })
+        })
+   
+
+
+
+
+
+
+
+ 
+  
+    // const clicked = true;
+    // addBtn.addEventListener('click', () => {
+    //     if (clicked === true) {
+    //         cartArr.push(product.price);
+    //         console.log(cartArr);
+    //     } 
+    // })
+}) 
+
+{/* <div class= "btnDiv"><button type= "button" value= "${product.price}" id="btnPro">Add</button> 
+<button type= "button" value= "${product.price}"id="btnProRem">Remove</button></div>   */}
+
 
